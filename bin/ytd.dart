d301397f620eae718e509a5efbb39489f97c81bc
Lucifer † <35570939+amannirala13@users.noreply.github.com>
2021-04-24 05:35:36 +0530

    added escaped time feature

--- bin/ytd.dart
+++ bin/ytd.dart
@@ -5,39 +5,44 @@ import 'dart:io';
 String url;
 var yt = YoutubeExplode();
 Video video;
+Stopwatch stopwatch;
 
 void main(List<String> arguments){
   if(arguments.isEmpty){print('Enter youtube video URL: ');
     url = stdin.readLineSync();}else{url = arguments[0];}loadVideoFromURL();
-}
-void loadVideoFromURL() {
+}void loadVideoFromURL() {
+  stopwatch = Stopwatch()..start();
   try {print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
         'Loading video... Please wait...\n'
         '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
     var videoRes = yt.videos.get(url);
     videoRes.then((value) => gotVideo(value));
-  } catch(e){print(e);}
-}
-void gotVideo(Video value) {
+  } catch(e){print(e);printEscapedTime();}
+}void gotVideo(Video value) {
   video = value;
   print('-------------------Video info--------------------------');
   print('Title: ${video.title}');
   print('Description: ${video.description}');
   print('Duration: ${video.duration}');
   print('Author: ${video.author}');
-  print('-------------------Video info--------------------------');
-  try{yt.videos.streamsClient.getManifest(url).then((value) => getStreamManifest(value));} catch(e){print(e);}
-}
-void getStreamManifest(StreamManifest value) async {
+  print('-------------------------------------------------------');
+  try{yt.videos.streamsClient.getManifest(url).then((value) => getStreamManifest(value));} catch(e){print(e);printEscapedTime();}
+}void getStreamManifest(StreamManifest value) async {
   try{var manifest = value;
   var streamInfo = manifest.muxed.sortByVideoQuality().last;
   if(streamInfo != null){var stream = yt.videos.streamsClient.get(streamInfo);
     var file = File('${video.title}.mp4');
     var filestream = file.openWrite();
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+    print('-----------------------------------------------------\n'
         'Downloading... Please wait...'
-        '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+        '\n-----------------------------------------------------');
     await stream.pipe(filestream);
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+    print('-----------------------------------------------------\n'
         'Finished Downloading. File saved as: ${video.title}.mp4\n'
-        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');}}catch(e){print(e);}}
+        '-----------------------------------------------------');
+    await filestream.flush(); await filestream.close();
+    printEscapedTime();
+  }}catch(e){print(e);printEscapedTime();}}
+void printEscapedTime(){
+  if(stopwatch.isRunning){print('Total Time escaped: ${stopwatch.elapsed}');
+    stopwatch.stop();}}
