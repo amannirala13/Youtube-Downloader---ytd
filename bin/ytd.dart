f04d2b53a7043feed8baf5118fc77f3e8408c9e8
Lucifer † <35570939+amannirala13@users.noreply.github.com>
2021-06-05 00:30:30 +0530

    Fixed google youtube api failing issue

--- bin/ytd.dart
+++ bin/ytd.dart
@@ -9,14 +9,14 @@ Stopwatch stopwatch;
 
 void main(List<String> arguments){
   if(arguments.isEmpty){print('Enter youtube video URL: ');
-    url = stdin.readLineSync();}else{url = arguments[0];}loadVideoFromURL();
+  url = stdin.readLineSync();}else{url = arguments[0];}loadVideoFromURL();
 }void loadVideoFromURL() {
   stopwatch = Stopwatch()..start();
   try {print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
-        'Loading video... Please wait...\n'
-        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
-    var videoRes = yt.videos.get(url);
-    videoRes.then((value) => gotVideo(value));
+      'Loading video... Please wait...\n'
+      '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+  var videoRes = yt.videos.get(url);
+  videoRes.then((value) => gotVideo(value));
   } catch(e){print(e);printEscapedTime();}
 }void gotVideo(Video value) {
   video = value;
@@ -31,18 +31,18 @@ void main(List<String> arguments){
   try{var manifest = value;
   var streamInfo = manifest.muxed.sortByVideoQuality().last;
   if(streamInfo != null){var stream = yt.videos.streamsClient.get(streamInfo);
-    var file = File('${video.title}.mp4');
-    var filestream = file.openWrite();
-    print('-----------------------------------------------------\n'
-        'Downloading... Please wait...'
-        '\n-----------------------------------------------------');
-    await stream.pipe(filestream);
-    print('-----------------------------------------------------\n'
-        'Finished Downloading. File saved as: ${video.title}.mp4\n'
-        '-----------------------------------------------------');
-    await filestream.flush(); await filestream.close();
-    printEscapedTime();
+  var file = File('${video.title}.mp4');
+  var filestream = file.openWrite();
+  print('-----------------------------------------------------\n'
+      'Downloading... Please wait...'
+      '\n-----------------------------------------------------');
+  await stream.pipe(filestream);
+  print('-----------------------------------------------------\n'
+      'Finished Downloading. File saved as: ${video.title}.mp4\n'
+      '-----------------------------------------------------');
+  await filestream.flush(); await filestream.close();
+  printEscapedTime();
   }}catch(e){print(e);printEscapedTime();}}
 void printEscapedTime(){
   if(stopwatch.isRunning){print('Total Time escaped: ${stopwatch.elapsed}');
-    stopwatch.stop();}}
+  stopwatch.stop();}}
