f3c6e201040b4f9e89bf2c3c81283ca6f9e36811
Lucifer † <35570939+amannirala13@users.noreply.github.com>
2021-07-26 22:50:32 +0530

    Fixed issues with api. Added option to select resolution

--- bin/ytd.dart
+++ bin/ytd.dart
@@ -33,20 +33,29 @@ void main(List<String> arguments){
   try{yt.videos.streamsClient.getManifest(url).then((value) => getStreamManifest(value));} catch(e){print(e);printEscapedTime();}
 }void getStreamManifest(StreamManifest value) async {
   try{var manifest = value;
-  var streamInfo = manifest.muxed.sortByVideoQuality().last;
-  if(streamInfo != null){var stream = yt.videos.streamsClient.get(streamInfo);
-  var file = File('${video.title}.mp4');
-  var filestream = file.openWrite();
-  print('-----------------------------------------------------\n'
-      'Downloading... Please wait...'
-      '\n-----------------------------------------------------');
-  await stream.pipe(filestream);
-  print('-----------------------------------------------------\n'
-      'Finished Downloading. File saved as: ${video.title}.mp4\n'
-      '-----------------------------------------------------');
-  await filestream.flush(); await filestream.close();
-  printEscapedTime();
-  }}catch(e){print(e);printEscapedTime();}}
+    var list = manifest.muxed.sortByVideoQuality();
+    var c = 0;
+    print('Choice   |   Quality   |   Size (MB)');
+    for(var i in list){
+      print(' [$c]  --> ${i.videoResolution} | ${i.videoCodec} | ${i.size.totalMegaBytes.round()}');
+      c++;
+    }
+    print('Enter your quality choice: ');
+    var choice = int.parse(stdin.readLineSync()!);
+    var streamInfo = manifest.muxed.sortByVideoQuality()[choice];
+    var stream = yt.videos.streamsClient.get(streamInfo);
+    var file = File('${video.title.toString().replaceAll(RegExp(r"[^\w]"), '_')}.mp4');
+    var filestream = file.openWrite();
+print('-----------------------------------------------------\n'
+    'Downloading... Please wait...'
+    '\n-----------------------------------------------------');
+await stream.pipe(filestream);
+print('-----------------------------------------------------\n'
+    'Finished Downloading. File saved as: ${video.title.toString().replaceAll(RegExp(r"[^\w]"), '_')}.mp4\n'
+    '-----------------------------------------------------');
+await filestream.flush(); await filestream.close();
+printEscapedTime();
+}catch(e){print(e);printEscapedTime();}}
 void printEscapedTime(){
   if(stopwatch.isRunning){print('Total Time escaped: ${stopwatch.elapsed}');
   stopwatch.stop();}}
